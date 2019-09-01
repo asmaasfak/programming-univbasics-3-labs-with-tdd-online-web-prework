c777fd8a29aaed444e7aab3707f59a7ced459aa3
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-01 21:34:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,6 @@
 # Add your variables here
 first_number = 10
-second_number
+second_number = 2
 
-def addition ( first_number , second_number)
- sum = first_number + second_number
- return sum
-end
-puts addition( 5 , 3)
+sum = first_number + second_number
+
